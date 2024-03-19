d2df57302fdd96a1f2181de90bae8aa383de5b81
devisriJune <163969469+devisriJune@users.noreply.github.com>
2024-03-19 15:18:00 +0530

    Update ProductDao.java

--- src/com/dao/ProductDao.java
+++ src/com/dao/ProductDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.model.Product;
 
-public  {
+public interface ProductDao {
 	//Dao package
 	    List<Product> getAllProducts();
 	    void addProduct(Product product);
